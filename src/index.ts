4f0a72f82db0f45be74dc4cef1d1454baffd6ab2
Enrique A <info@earrieta.dev>
2024-02-07 16:30:39 -0300

    Bring back the walletconnect module

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,4 @@ export * from './modules/xbull/xbull.module';
 export * from './modules/freighter/freighter.module';
 export * from './modules/albedo/albedo.module';
 export * from './modules/rabet/rabet.module';
+export * from './modules/walletconnect/walletconnect.module';
