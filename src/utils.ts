9f51a1dc5f6e792cd971847f615d7b04164fa5fa
Ting Tze Chuen <hello@ting.rocks>
2024-06-04 16:45:19 +0800

    allowAllModules include Hana module

--- src/utils.ts
+++ src/utils.ts
@@ -3,8 +3,16 @@ import { FreighterModule } from './modules/freighter/freighter.module';
 import { LobstrModule } from './modules/lobstr/lobstr.module';
 import { RabetModule } from './modules/rabet/rabet.module';
 import { xBullModule } from './modules/xbull/xbull.module';
+import { HanaModule } from './modules/hana/hana.module';
 import { ModuleInterface } from './types';
 
 export function allowAllModules(): ModuleInterface[] {
-  return [new AlbedoModule(), new FreighterModule(), new RabetModule(), new xBullModule(), new LobstrModule()];
+  return [
+    new AlbedoModule(),
+    new FreighterModule(),
+    new RabetModule(),
+    new xBullModule(),
+    new LobstrModule(),
+    new HanaModule(),
+  ];
 }
