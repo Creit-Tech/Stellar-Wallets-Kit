31eef171b844aad34776be7caee5ffe7e49ae61b
Enrique A <info@earrieta.dev>
2025-06-03 13:07:38 -0400

    Add `sep43Modules` and a `filterBy` parameter for both `allowAllModules` and `sep43Modules`

--- src/utils.ts
+++ src/utils.ts
@@ -7,8 +7,12 @@ import { HotWalletModule } from './modules/hotwallet.module';
 import { HanaModule } from './modules/hana.module';
 import { ModuleInterface } from './types';
 
-export function allowAllModules(): ModuleInterface[] {
-  return [
+/**
+ * This method returns all modules that don't require extra configuration before they can be loaded
+ * You can provide a filter function if needed
+ */
+export function allowAllModules(opts?: { filterBy: (module: ModuleInterface) => boolean }): ModuleInterface[] {
+  const modules: ModuleInterface[] = [
     new AlbedoModule(),
     new FreighterModule(),
     new RabetModule(),
@@ -17,6 +21,16 @@ export function allowAllModules(): ModuleInterface[] {
     new HanaModule(),
     new HotWalletModule(),
   ];
+  return opts?.filterBy ? modules.filter(opts.filterBy) : modules;
+}
+
+/**
+ * This method only returns those modules from wallet that follow exactly the SEP-43 standard and don't require extra configuration before they can be loaded
+ * You can provide a filter function if needed
+ */
+export function sep43Modules(opts?: { filterBy: (module: ModuleInterface) => boolean }): ModuleInterface[] {
+  const modules: ModuleInterface[] = [new FreighterModule(), new HotWalletModule()];
+  return opts?.filterBy ? modules.filter(opts.filterBy) : modules;
 }
 
 export function parseError(e: any) {
